e6f39d6af7e6e259f100d4775a2295fa4e913ecb
Aleksandra Popko <112278780+alexandrabakhar@users.noreply.github.com>
2023-08-02 15:38:09 +0300

    hw2 (#3)
    * added index.html
    
    * added first screen
    
    * added first screen
    
    * Added js, created choosing the difficulty level.

---
+++ JS/events.js
@@ -0,0 +1,16 @@
+function selectGameLevel (event) {
+    event.preventDefault();
+
+    const difficultyLevels = blockDifficulty.querySelector('.difficulty__levels');
+
+    const selectedLevel = difficultyLevels.querySelector('input:checked');
+    if (!selectedLevel) {
+        console.log('Выберите уровень!');
+        return
+    }
+
+    window.application.level = selectedLevel.value;
+
+    document.body.textContent = '';
+}
+
